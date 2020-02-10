999e7dddb074eb12e84f8bfe6f8dc9c8307a1b51
asif ar-rahmah <rimbaborne@gmail.com>
2020-02-10 10:51:54 +0800

    user login
    migrasi dan database

--- database/seeds/Auth/UserTableSeeder.php
+++ database/seeds/Auth/UserTableSeeder.php
@@ -19,28 +19,28 @@ class UserTableSeeder extends Seeder
 
         // Add the master administrator, user id of 1
         User::create([
-            'first_name' => 'Admin',
-            'last_name' => 'Istrator',
-            'email' => 'admin@admin.com',
-            'password' => 'secret',
+            'first_name' => 'Super',
+            'last_name' => 'Admin',
+            'email' => 'admin@aims-balikpapan.com',
+            'password' => '1234',
             'confirmation_code' => md5(uniqid(mt_rand(), true)),
             'confirmed' => true,
         ]);
 
         User::create([
-            'first_name' => 'Backend',
-            'last_name' => 'User',
-            'email' => 'executive@executive.com',
-            'password' => 'secret',
+            'first_name' => 'User',
+            'last_name' => 'Manager',
+            'email' => 'manager@aims-balikpapan.com',
+            'password' => '1234',
             'confirmation_code' => md5(uniqid(mt_rand(), true)),
             'confirmed' => true,
         ]);
 
         User::create([
-            'first_name' => 'Default',
-            'last_name' => 'User',
-            'email' => 'user@user.com',
-            'password' => 'secret',
+            'first_name' => 'User',
+            'last_name' => 'Administrasi',
+            'email' => 'user@aims-balikpapan.com',
+            'password' => '1234',
             'confirmation_code' => md5(uniqid(mt_rand(), true)),
             'confirmed' => true,
         ]);
