d343b37c37f191d0b11a806cd27c063c8f0543a3
Gagang R. B <rimbaborne@gmail.com>
2021-04-13 14:01:59 +0800

    link profile di daftar baru ketinggalan

--- resources/views/backend/tahsin/daftar-baru.blade.php
+++ resources/views/backend/tahsin/daftar-baru.blade.php
@@ -155,7 +155,7 @@
                                     {{ $key + $tahsins->firstItem() }}
                                 </td>
                                 <td>
-                                    <a href="/admin/tahsin/{{ $tahsin->id }}/edit" style="color: rgb(56, 56, 56);">
+                                    <a href="/admin/tahsins/{{ $tahsin->id }}/edit" style="color: rgb(56, 56, 56);">
                                         <div style="text-transform: uppercase;">{{ $tahsin->nama_peserta }}</div>
                                         <div class="small text-muted">
                                             {{ $tahsin->no_tahsin }} | {{ $tahsin->nohp_peserta }}
