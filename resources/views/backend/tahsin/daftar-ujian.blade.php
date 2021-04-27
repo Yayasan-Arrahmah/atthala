af2db0d483a56ab98f085335a5c363b5af10c224
Gagang R. B <rimbaborne@gmail.com>
2021-04-27 11:40:40 +0800

    --

--- resources/views/backend/tahsin/daftar-ujian.blade.php
+++ resources/views/backend/tahsin/daftar-ujian.blade.php
@@ -94,7 +94,7 @@
                                 <a href="#" style="color: rgb(56, 56, 56);">
                                     <div style="text-transform: uppercase; font-weight: 700">{{ $pesertaujian->no_tahsin }} - {{ $pesertaujian->data->nama_peserta ?? '' }}</div>
                                     <div class="small text-muted">
-                                        {{ $pesertaujian->data->nohp_peserta ?? '' }} |  {{ $pesertaujian->data->waktu_lahir_peserta ?? '' }} | {{ \Carbon\Carbon::createFromFormat('d-m-Y', $pesertaujian->data->waktu_lahir_peserta ?? '')->age ?? '' }} Tahun
+                                        {{ $pesertaujian->data->nohp_peserta ?? '' }} |  {{ $pesertaujian->data->waktu_lahir_peserta ?? '' }} | {{ \Carbon\Carbon::createFromFormat('d-m-Y', $pesertaujian->data->waktu_lahir_peserta ?? '01-01-1901')->age ?? '' }} Tahun
                                     </div>
                                 </a>
                             </td>
@@ -105,7 +105,7 @@
                                 </div>
                             </td>
                             <td>
-                                {{ \Carbon\Carbon::createFromFormat('d-m-Y', $pesertaujian->data->waktu_lahir_peserta ?? '')->format('md') }}
+                                {{ \Carbon\Carbon::createFromFormat('d-m-Y', $pesertaujian->data->waktu_lahir_peserta ?? '01-01-1901')->format('md') }}
                             </td>
                             <td>
                                 <div class="text-center">
