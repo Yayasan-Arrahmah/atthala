dd6618cffb0d8b2243f6df060ae9ac44c337e86d
Gagang R. B <rimbaborne@gmail.com>
2021-10-25 08:54:08 +0800

    --

--- resources/views/backend/tahsin/daftar-ujian.blade.php
+++ resources/views/backend/tahsin/daftar-ujian.blade.php
@@ -101,7 +101,7 @@
                         @endphp
                         @foreach($pesertaujians as $key => $pesertaujian)
                         @php
-                            $data = DB::table('tahsins')->where('no_tahsin', $pesertaujian->no_tahsin)->where('angkatan_peserta', 18)->first();
+                            $data = DB::table('tahsins')->where('no_tahsin', $pesertaujian->no_tahsin)->where('angkatan_peserta', request()->angkatan ?? 18)->first();
                         @endphp
                         <tr>
                             <td class="text-center" >
