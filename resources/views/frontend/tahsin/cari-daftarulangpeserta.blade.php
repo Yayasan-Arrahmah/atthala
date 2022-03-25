d4741e7aab82e8acf4ddc5e73b107752bf32a602
Gagang R. B <rimbaborne@gmail.com>
2022-03-25 10:33:38 +0800

    tajwidi update link

--- resources/views/frontend/tahsin/cari-daftarulangpeserta.blade.php
+++ resources/views/frontend/tahsin/cari-daftarulangpeserta.blade.php
@@ -69,11 +69,12 @@
                                             <div class="row kotak">
                                                 <div class="col-2 text-center">
                                                     @if (strpos($tahsin->kenaikan_level_peserta, 'TAJWIDI') !== false )
-                                                        @if ($tahsin->jenis_peserta == 'IKHWAN')
+                                                        {{-- @if ($tahsin->jenis_peserta == 'IKHWAN')
                                                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSftUK5M8aWT1M2zDvb4HMXjWN4_BlTaUs6GuDCIB7DlOCUVng/viewform"  style="color:white; font-size: 11px" class="btn btn-primary">DAFTAR ULANG</a>
                                                         @else
                                                             <a href="https://docs.google.com/forms/d/e/1FAIpQLSftUK5M8aWT1M2zDvb4HMXjWN4_BlTaUs6GuDCIB7DlOCUVng/viewform"  style="color:white; font-size: 11px" class="btn btn-primary">DAFTAR ULANG</a>
-                                                        @endif
+                                                        @endif --}}
+                                                        <a href="https://forms.gle/rnK6SH4nJcck6LUc7"  style="color:white; font-size: 11px" class="btn btn-primary">DAFTAR ULANG</a>
                                                     @else
                                                             <a href="/tahsin/daftar-ulang-peserta-XX/daftar?id={{ $tahsin->no_tahsin }}&idt={{ $tahsin->id }}&nama={{ $tahsin->nama_peserta }}" style="color:white; font-size: 11px" class="btn btn-primary">DAFTAR ULANG</a>
                                                     @endif
