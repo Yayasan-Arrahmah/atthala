c9d960217a2661ebd4fe4fc3981b726f24c053dc
rimbaborne <gagangborne@gmail.com>
2022-10-03 06:47:21 +0800

    tes statistik tahsin

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -32,117 +32,117 @@ class DashboardController extends Controller
                             ->get();
         $datalevel     = LevelTahsin::orderBy('id', 'asc')->get();
 
-        foreach ($d_angkatan as $data_total) {
-            $total_angkatan[]                     = (int)$data_total->angkatan_peserta;
-            $total_peserta[]                      = Tahsin::whereNotNull('level_peserta')->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_daftar_baru[]          = Tahsin::daftarBaru($data_total->angkatan_peserta)->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_daftar_ulang[]         = Tahsin::whereNotNull('level_peserta')->daftarUlang($data_total->angkatan_peserta)->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_tidak_daftar_ulang[]   = Tahsin::whereNotNull('level_peserta')->tidakDaftarUlang($data_total->angkatan_peserta-1)->angkatan($data_total->angkatan_peserta-1)->count();
-            $total_peserta_tidak_ujian[]          = Tahsin::whereNotNull('level_peserta')->whereNull('kenaikan_level_peserta')->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_tidak_naik_level[]     = Tahsin::whereNotNull('level_peserta')->whereRaw('tahsins.level_peserta != tahsins.kenaikan_level_peserta')->whereNotNull('kenaikan_level_peserta')->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_ikhwan[]               = Tahsin::whereNotNull('level_peserta')->ikhwan()->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_akhwat[]               = Tahsin::whereNotNull('level_peserta')->akhwat()->angkatan($data_total->angkatan_peserta)->count();
-            $total_peserta_alhaq[]                = Tahsin::whereNotNull('level_peserta')->where('kenaikan_level_peserta', 'TAJWIDI 1')->angkatan($data_total->angkatan_peserta)->count();
-        }
+        // foreach ($d_angkatan as $data_total) {
+        //     $total_angkatan[]                     = (int)$data_total->angkatan_peserta;
+        //     $total_peserta[]                      = Tahsin::whereNotNull('level_peserta')->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_daftar_baru[]          = Tahsin::daftarBaru($data_total->angkatan_peserta)->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_daftar_ulang[]         = Tahsin::whereNotNull('level_peserta')->daftarUlang($data_total->angkatan_peserta)->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_tidak_daftar_ulang[]   = Tahsin::whereNotNull('level_peserta')->tidakDaftarUlang($data_total->angkatan_peserta-1)->angkatan($data_total->angkatan_peserta-1)->count();
+        //     $total_peserta_tidak_ujian[]          = Tahsin::whereNotNull('level_peserta')->whereNull('kenaikan_level_peserta')->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_tidak_naik_level[]     = Tahsin::whereNotNull('level_peserta')->whereRaw('tahsins.level_peserta != tahsins.kenaikan_level_peserta')->whereNotNull('kenaikan_level_peserta')->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_ikhwan[]               = Tahsin::whereNotNull('level_peserta')->ikhwan()->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_akhwat[]               = Tahsin::whereNotNull('level_peserta')->akhwat()->angkatan($data_total->angkatan_peserta)->count();
+        //     $total_peserta_alhaq[]                = Tahsin::whereNotNull('level_peserta')->where('kenaikan_level_peserta', 'TAJWIDI 1')->angkatan($data_total->angkatan_peserta)->count();
+        // }
 
         $statistik_utama = [
-            'total_angkatan'                   => $total_angkatan ?? 0,
-            'total_peserta'                    => $total_peserta ?? 0,
-            'total_peserta_daftar_baru'        => $total_peserta_daftar_baru ?? 0,
-            'total_peserta_daftar_ulang'       => $total_peserta_daftar_ulang ?? 0,
-            'total_peserta_tidak_daftar_ulang' => $total_peserta_tidak_daftar_ulang ?? 0,
-            'total_peserta_tidak_ujian'        => $total_peserta_tidak_ujian ?? 0,
-            'total_peserta_tidak_naik_level'   => $total_peserta_tidak_naik_level ?? 0,
-            'total_peserta_ikhwan'             => $total_peserta_ikhwan ?? 0,
-            'total_peserta_akhwat'             => $total_peserta_akhwat ?? 0,
-            'total_peserta_alhaq'              => $total_peserta_alhaq ?? 0,
+            // 'total_angkatan'                   => $total_angkatan ?? 0,
+            // 'total_peserta'                    => $total_peserta ?? 0,
+            // 'total_peserta_daftar_baru'        => $total_peserta_daftar_baru ?? 0,
+            // 'total_peserta_daftar_ulang'       => $total_peserta_daftar_ulang ?? 0,
+            // 'total_peserta_tidak_daftar_ulang' => $total_peserta_tidak_daftar_ulang ?? 0,
+            // 'total_peserta_tidak_ujian'        => $total_peserta_tidak_ujian ?? 0,
+            // 'total_peserta_tidak_naik_level'   => $total_peserta_tidak_naik_level ?? 0,
+            // 'total_peserta_ikhwan'             => $total_peserta_ikhwan ?? 0,
+            // 'total_peserta_akhwat'             => $total_peserta_akhwat ?? 0,
+            // 'total_peserta_alhaq'              => $total_peserta_alhaq ?? 0,
         ];
 
         // DETAIL CHART
-        // $peserta_daftar_baru          = Tahsin::daftarBaru($this->angkatan)->angkatan($this->angkatan)->count();
-        // $peserta_daftar_ulang         = Tahsin::whereNotNull('level_peserta')->daftarUlang($this->angkatan)->angkatan($this->angkatan)->count();
-        // $peserta_tidak_daftar_ulang   = Tahsin::whereNotNull('level_peserta')->tidakDaftarUlang($this->angkatan-1)->angkatan($this->angkatan-1)->count();
-        // $peserta_ikhwan               = Tahsin::whereNotNull('level_peserta')->ikhwan()->angkatan($this->angkatan)->count();
-        // $peserta_akhwat               = Tahsin::whereNotNull('level_peserta')->akhwat()->angkatan($this->angkatan)->count();
-        // $peserta_naik_level           = Tahsin::whereRaw('tahsins.level_peserta = tahsins.kenaikan_level_peserta')
-        //                                 ->whereNotNull('level_peserta')
-        //                                 ->whereNotNull('kenaikan_level_peserta')
-        //                                 ->angkatan($this->angkatan)->count();
-        // $peserta_tidak_naik_level     = Tahsin::whereRaw('tahsins.level_peserta != tahsins.kenaikan_level_peserta')
-        //                                 ->whereNotNull('level_peserta')
-        //                                 ->whereNotNull('kenaikan_level_peserta')
-        //                                 ->angkatan($this->angkatan)->count();
-        // $peserta_ujian                = Tahsin::whereNotNull('kenaikan_level_peserta')
-        //                                 ->angkatan($this->angkatan)->count();
-        // $peserta_tidak_ujian          = Tahsin::whereNotNull('level_peserta')
-        //                                 ->whereNull('kenaikan_level_peserta')
-        //                                 ->angkatan($this->angkatan)->count();
-        // $peserta_aktif                = Tahsin::whereNotNull('level_peserta')->angkatan($this->angkatan)->get();
+        $peserta_daftar_baru          = Tahsin::daftarBaru($this->angkatan)->angkatan($this->angkatan)->count();
+        $peserta_daftar_ulang         = Tahsin::whereNotNull('level_peserta')->daftarUlang($this->angkatan)->angkatan($this->angkatan)->count();
+        $peserta_tidak_daftar_ulang   = Tahsin::whereNotNull('level_peserta')->tidakDaftarUlang($this->angkatan-1)->angkatan($this->angkatan-1)->count();
+        $peserta_ikhwan               = Tahsin::whereNotNull('level_peserta')->ikhwan()->angkatan($this->angkatan)->count();
+        $peserta_akhwat               = Tahsin::whereNotNull('level_peserta')->akhwat()->angkatan($this->angkatan)->count();
+        $peserta_naik_level           = Tahsin::whereRaw('tahsins.level_peserta = tahsins.kenaikan_level_peserta')
+                                        ->whereNotNull('level_peserta')
+                                        ->whereNotNull('kenaikan_level_peserta')
+                                        ->angkatan($this->angkatan)->count();
+        $peserta_tidak_naik_level     = Tahsin::whereRaw('tahsins.level_peserta != tahsins.kenaikan_level_peserta')
+                                        ->whereNotNull('level_peserta')
+                                        ->whereNotNull('kenaikan_level_peserta')
+                                        ->angkatan($this->angkatan)->count();
+        $peserta_ujian                = Tahsin::whereNotNull('kenaikan_level_peserta')
+                                        ->angkatan($this->angkatan)->count();
+        $peserta_tidak_ujian          = Tahsin::whereNotNull('level_peserta')
+                                        ->whereNull('kenaikan_level_peserta')
+                                        ->angkatan($this->angkatan)->count();
+        $peserta_aktif                = Tahsin::whereNotNull('level_peserta')->angkatan($this->angkatan)->get();
 
 
-        // foreach ($datalevel as $d_level) {
-        //     $statistik_level[] = [
-        //         strval($d_level->nama) => Tahsin::where('level_peserta', $d_level->nama)->angkatan($this->angkatan)->count(),
-        //     ];
-        // }
+        foreach ($datalevel as $d_level) {
+            $statistik_level[] = [
+                strval($d_level->nama) => Tahsin::where('level_peserta', $d_level->nama)->angkatan($this->angkatan)->count(),
+            ];
+        }
 
-        // foreach ($datalevel as $d_level_daftar_baru) {
-        //     $statistik_level_daftar_baru[] = [
-        //         strval($d_level_daftar_baru->nama) => Tahsin::daftarBaru($this->angkatan)->where('level_peserta', $d_level_daftar_baru->nama)->angkatan($this->angkatan)->count(),
-        //     ];
-        // }
+        foreach ($datalevel as $d_level_daftar_baru) {
+            $statistik_level_daftar_baru[] = [
+                strval($d_level_daftar_baru->nama) => Tahsin::daftarBaru($this->angkatan)->where('level_peserta', $d_level_daftar_baru->nama)->angkatan($this->angkatan)->count(),
+            ];
+        }
 
-        // foreach ($datalevel as $d_level_daftar_ulang) {
-        //     $statistik_level_daftar_ulang[] = [
-        //         strval($d_level_daftar_ulang->nama) => Tahsin::daftarUlang($this->angkatan)->where('level_peserta', $d_level_daftar_ulang->nama)->angkatan($this->angkatan)->count(),
-        //     ];
-        // }
+        foreach ($datalevel as $d_level_daftar_ulang) {
+            $statistik_level_daftar_ulang[] = [
+                strval($d_level_daftar_ulang->nama) => Tahsin::daftarUlang($this->angkatan)->where('level_peserta', $d_level_daftar_ulang->nama)->angkatan($this->angkatan)->count(),
+            ];
+        }
 
-        // $a = 0;
-        // $b = 0;
-        // $c = 0;
-        // $d = 0;
-        // $e = 0;
-        // $f = 0;
-        // foreach ($peserta_aktif as $data_k){
-        //     if ($data_k->waktu_lahir_peserta) {
-        //         $kategori_umur = Carbon::createFromFormat('d-m-Y', $data_k->waktu_lahir_peserta)->age;
-        //         $tes[] =  $kategori_umur;
-        //         if ($kategori_umur < 17) {
-        //             $a++;
-        //         } elseif ($kategori_umur >= 17 && $kategori_umur <= 25) {
-        //             $b++;
-        //         } elseif ($kategori_umur >= 26 && $kategori_umur <= 35) {
-        //             $c++;
-        //         } elseif ($kategori_umur >= 36 && $kategori_umur <= 45) {
-        //             $d++;
-        //         } elseif ($kategori_umur >= 46 && $kategori_umur <= 55) {
-        //             $e++;
-        //         } elseif ($kategori_umur > 55) {
-        //             $f++;
-        //         }
-        //     }
-        // }
+        $a = 0;
+        $b = 0;
+        $c = 0;
+        $d = 0;
+        $e = 0;
+        $f = 0;
+        foreach ($peserta_aktif as $data_k){
+            if ($data_k->waktu_lahir_peserta) {
+                $kategori_umur = Carbon::createFromFormat('d-m-Y', $data_k->waktu_lahir_peserta)->age;
+                $tes[] =  $kategori_umur;
+                if ($kategori_umur < 17) {
+                    $a++;
+                } elseif ($kategori_umur >= 17 && $kategori_umur <= 25) {
+                    $b++;
+                } elseif ($kategori_umur >= 26 && $kategori_umur <= 35) {
+                    $c++;
+                } elseif ($kategori_umur >= 36 && $kategori_umur <= 45) {
+                    $d++;
+                } elseif ($kategori_umur >= 46 && $kategori_umur <= 55) {
+                    $e++;
+                } elseif ($kategori_umur > 55) {
+                    $f++;
+                }
+            }
+        }
 
-        // $statistik = [
-        //     'peserta_daftar_baru'        => $peserta_daftar_baru ?? 0,
-        //     'peserta_daftar_ulang'       => $peserta_daftar_ulang ?? 0,
-        //     'peserta_tidak_daftar_ulang' => $peserta_tidak_daftar_ulang ?? 0,
-        //     'peserta_ikhwan'             => $peserta_ikhwan ?? 0,
-        //     'peserta_akhwat'             => $peserta_akhwat ?? 0,
-        //     'peserta_naik_level'         => $peserta_naik_level ?? 0,
-        //     'peserta_tidak_naik_level'   => $peserta_tidak_naik_level ?? 0,
-        //     'peserta_ujian'              => $peserta_ujian ?? 0,
-        //     'peserta_tidak_ujian'        => $peserta_tidak_ujian ?? 0,
-        //     'peserta_umur_1'             => $a,
-        //     'peserta_umur_2'             => $b,
-        //     'peserta_umur_3'             => $c,
-        //     'peserta_umur_4'             => $d,
-        //     'peserta_umur_5'             => $e,
-        //     'peserta_umur_6'             => $f,
-        // ];
+        $statistik = [
+            'peserta_daftar_baru'        => $peserta_daftar_baru ?? 0,
+            'peserta_daftar_ulang'       => $peserta_daftar_ulang ?? 0,
+            'peserta_tidak_daftar_ulang' => $peserta_tidak_daftar_ulang ?? 0,
+            'peserta_ikhwan'             => $peserta_ikhwan ?? 0,
+            'peserta_akhwat'             => $peserta_akhwat ?? 0,
+            'peserta_naik_level'         => $peserta_naik_level ?? 0,
+            'peserta_tidak_naik_level'   => $peserta_tidak_naik_level ?? 0,
+            'peserta_ujian'              => $peserta_ujian ?? 0,
+            'peserta_tidak_ujian'        => $peserta_tidak_ujian ?? 0,
+            'peserta_umur_1'             => $a,
+            'peserta_umur_2'             => $b,
+            'peserta_umur_3'             => $c,
+            'peserta_umur_4'             => $d,
+            'peserta_umur_5'             => $e,
+            'peserta_umur_6'             => $f,
+        ];
 
         return view('backend.pendidikan.tahsin.dashboard',
-            compact('dataangkatan', 'statistik_utama','datalevel'));
+            compact('dataangkatan', 'statistik_utama', 'statistik', 'statistik_level', 'statistik_level_daftar_baru', 'statistik_level_daftar_ulang', 'datalevel'));
     }
 }
--- resources/views/backend/pendidikan/tahsin/dashboard.blade.php
+++ resources/views/backend/pendidikan/tahsin/dashboard.blade.php
@@ -44,7 +44,7 @@
     </div><!--col-->
 </div><!--row-->
 
-<div class="row ">
+{{-- <div class="row ">
     <div class="col">
         <div class="card">
             <div class="card-body">
@@ -59,7 +59,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 
 <div class="row ">
     <div class="col">
@@ -114,7 +114,7 @@
 </div>
 
 {{-- CHART 1 - DATA SELURUH ANGKATAN--}}
-<script>
+{{-- <script>
     var options = {
         series: [
         {
@@ -199,10 +199,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart1"), options);
     chart.render();
-</script>
+</script> --}}
 
 {{-- CHART 2 - DATA JENIS PESERTA--}}
-{{-- <script>
+<script>
     var options = {
         series: [{{ json_encode(data_get($statistik, 'peserta_ikhwan')) }}, {{ json_encode(data_get($statistik, 'peserta_akhwat')) }}],
         chart: {
@@ -247,10 +247,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart2"), options);
     chart.render();
-</script> --}}
+</script>
 
 {{-- CHART 3 - DATA KENAIKAN PESERTA--}}
-{{-- <script>
+<script>
     var options = {
         series: [{{ json_encode(data_get($statistik, 'peserta_naik_level')) }}, {{ json_encode(data_get($statistik, 'peserta_tidak_naik_level')) }}],
         chart: {
@@ -294,10 +294,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart3"), options);
     chart.render();
-</script> --}}
+</script>
 
 {{-- CHART 4 - DATA LEVEL --}}
-{{-- <script>
+<script>
     var options = {
         series: [{
             name: 'Daftar Baru',
@@ -367,10 +367,10 @@
     var chart = new ApexCharts(document.querySelector("#chart4"), options);
     chart.render();
 
-</script> --}}
+</script>
 
 {{-- CHART 5 - DAFTAR ULANG PESERTA--}}
-{{-- <script>
+<script>
     var options = {
         series: [{{ json_encode(data_get($statistik, 'peserta_daftar_ulang')) }}, {{ json_encode(data_get($statistik, 'peserta_tidak_daftar_ulang')) }}],
         chart: {
@@ -414,10 +414,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart5"), options);
     chart.render();
-</script> --}}
+</script>
 
 {{-- CHART 6 - DAFTAR UJIAN PESERTA--}}
-{{-- <script>
+<script>
     var options = {
         series: [{{ json_encode(data_get($statistik, 'peserta_ujian')) }}, {{ json_encode(data_get($statistik, 'peserta_tidak_ujian')) }}],
         chart: {
@@ -461,10 +461,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart6"), options);
     chart.render();
-</script> --}}
+</script>
 
 {{-- CHART 7 - DATA MARGIN SELURUH ANGKATAN--}}
-<script>
+{{-- <script>
     var options = {
         series: [
         {
@@ -533,10 +533,10 @@
 
     var chart = new ApexCharts(document.querySelector("#chart7"), options);
     chart.render();
-</script>
+</script> --}}
 
 {{-- CHART 8 - DATA UMUR PESERTA--}}
-{{-- <script>
+<script>
     var options = {
         series: [{
             data: [
@@ -589,6 +589,6 @@
     var chart = new ApexCharts(document.querySelector("#chart8"), options);
     chart.render();
 
-</script> --}}
+</script>
 
 @endsection
