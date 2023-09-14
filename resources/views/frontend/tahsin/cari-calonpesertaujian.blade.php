46113a833240cacf03ab1d7010bbe92e9450f6f4
rimbaborne <gagangborne@gmail.com>
2023-09-14 10:29:22 +0800

    Update cari-calonpesertaujian.blade.php

--- resources/views/frontend/tahsin/cari-calonpesertaujian.blade.php
+++ resources/views/frontend/tahsin/cari-calonpesertaujian.blade.php
@@ -3,13 +3,13 @@
 @section('title', app_name() . ' | ' . __('labels.frontend.auth.login_box_title'))
 
 @section('content')
-<div class="row justify-content-center align-items-center">
+{{-- <div class="row justify-content-center align-items-center">
     <div class="col col-sm-12 align-self-center">
         <div class="card">
             <center>
                 <img class="navbar-brand-full" src="{{ asset('img/logo-lttq.jpeg') }}" width="150" alt="arrahmah">
                 <div style="padding-top: 0px">
-                    <h4>Pendaftaran Calon Peserta Ujian Tahsin <br> Belum Dibuka</h4>
+                    <h4>Pendaftaran Calon Peserta Ujian Tahsin <br> Telah Ditutup</h4>
                     <div class="text-muted">
                         Angkatan {{ session('daftar_ujian') }}
                     </div>
@@ -17,8 +17,8 @@
             </center>
         </div>
     </div>
-</div>
-{{-- <div class="row justify-content-center align-items-center">
+</div> --}}
+<div class="row justify-content-center align-items-center">
     <div class="col col-sm-12 align-self-center">
         <div class="card">
             <center>
@@ -97,5 +97,5 @@
         </div><!--card-->
 
     </div><!-- col-md-8 -->
-</div><!-- row --> --}}
+</div><!-- row -->
 @endsection
