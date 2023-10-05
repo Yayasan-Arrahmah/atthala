72bacad86877c5544b530b878be8672453d23d5d
rimbaborne <gagangborne@gmail.com>
2023-10-05 16:17:11 +0800

    Update DashboardController.php

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -26,7 +26,7 @@ class DashboardController extends Controller
 
      public function __construct()
     {
-        $this->angkatan_tahsin  = 22;
+        $this->angkatan_tahsin  = 23;
     }
 
     public function index()
